9928b510dcd79d02aa883d940ebdbc77042762f5
Kate <korsunekaterinaa@gmail.com>
2023-04-20 02:49:22 +0300

    Edited delete_text function

--- text.py
+++ text.py
@@ -1,10 +1,18 @@
 def delete_text(text):
     i = 0
+    arr = []
     while i < len(text):
         if text[i] == '(':
-            while text[i] != ')':
-                text = text[:i] + text[i+1:]
+            arr.append('(')
             text = text[:i] + text[i + 1:]
-        i = i + 1
+        if text[i] == ')':
+            arr.pop()
+            text = text[:i] + text[i + 1:]
+        if len(arr) != 0:
+            text = text[:i] + text[i + 1:]
+        else:
+            i = i + 1
     return text
+
+
 
